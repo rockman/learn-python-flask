878ebdcc0eb519f6b96f0767474f8e6b720b4703
Kaya Koch <Kaya.Koch@gmail.com>
2021-10-30 12:50:29 +1100

    Completed module 5: add and remove cards

--- pluralsight-flask-getting-started/flashcards/flashcards.py
+++ pluralsight-flask-getting-started/flashcards/flashcards.py
@@ -1,6 +1,7 @@
 
+from flask.helpers import url_for
 from models import db
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request, redirect
 
 app = Flask(__name__)
 
@@ -22,9 +23,38 @@ def card(index):
     except IndexError:
         abort(404)
 
+
 @app.route('/api/hint/<int:index>')
 def hint(index):
     try:
         return db[index]['question']
     except IndexError:
         abort(404)
+
+
+@app.route('/add_card')
+def add_card():
+    return render_template('add_card.html')
+
+
+@app.post('/add_card')
+def do_add_card():
+    try:
+        db.append(dict(
+            question=request.form['question'],
+            answer=request.form['answer']
+        ))
+    except:
+        pass
+    return redirect(url_for('card', index=len(db) - 1))
+
+
+@app.route('/remove/<int:index>')
+def remove(index):
+    return render_template('remove_card.html', card=db[index], index=index)
+
+
+@app.post('/remove/<int:index>')
+def do_remove(index):
+    del db[index]
+    return redirect(url_for('welcome'))
