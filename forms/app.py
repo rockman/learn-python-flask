b80ae70842d88c9d91f5b15aeeac1d9e1f9c11bc
Kaya Koch <Kaya.Koch@gmail.com>
2021-12-04 21:27:51 +1100

    [forms] Added select fields where one depends on the other

--- forms/app.py
+++ forms/app.py
@@ -1,7 +1,7 @@
 
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, request, abort, jsonify
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SelectField
 from wtforms.validators import InputRequired, DataRequired
 
 
@@ -9,8 +9,25 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'some secret key'
 
 
+category_data = (
+    (23, 'alpha'),
+    (42, 'beta'),
+)
+
+subcategory_data = (
+    (23, 1, 'aaa'),
+    (23, 2, 'bbb'),
+    (42, 7, 'foo'),
+    (42, 8, 'bar'),
+)
+
+blank_option = (-1, '---')
+
+
 class BasicForm(FlaskForm):
     name = StringField('Name', validators=[InputRequired('Name is required'), DataRequired('Name is empty')])
+    category = SelectField('Category', coerce=int)
+    subcategory = SelectField('Subcategory', coerce=int)
 
 
 @app.route('/')
@@ -22,7 +39,11 @@ def home():
 def basic():
     form = BasicForm()
 
+    form.category.choices = list(category_data)
+    form.subcategory.choices = get_subcategories_for_category(form.category.choices[0][0])
+
     if form.validate_on_submit():
+        print(f'category={form.category.data}({type(form.category.data)}) subcategory={form.subcategory.data}({type(form.subcategory.data)})')
         flash('Form ok!', 'info')
         return redirect(url_for('basic'))
 
@@ -30,3 +51,22 @@ def basic():
         flash('Errors exist', 'error')
 
     return render_template('basic.html', form=form)
+
+
+@app.route('/api/categories/')
+def categories():
+    return jsonify(category_data)
+
+
+@app.route('/api/categories/<int:category>')
+@app.route('/api/categories/<int:category>/subcategories')
+def subcategories(category):
+    options = get_subcategories_for_category(category)
+    if not options:
+        return abort(404)
+
+    return jsonify(options)
+
+
+def get_subcategories_for_category(category):
+    return [i[1:] for i in subcategory_data if i[0] == category]
--- forms/static/app.js
+++ forms/static/app.js
@@ -1,1 +1,47 @@
-// nothing yet
+
+var categoryElement = document.getElementById('category');
+var subcategoryElement = document.getElementById('subcategory');
+
+if (categoryElement) {
+
+    function disableElements() {
+        categoryElement.disabled = true;
+        subcategoryElement.disabled = true;
+    }
+
+    function enableElements() {
+        categoryElement.disabled = false;
+        subcategoryElement.disabled = false;
+    }
+
+    function setSubcategories(options) {
+        subcategoryElement.innerHTML = '';
+
+        for(let i = 0; i < options.length; ++i) {
+            let option = options[i];
+            let optionElement = document.createElement('option');
+            optionElement.value = option[0];
+            optionElement.innerText = option[1];
+            subcategoryElement.append(optionElement);
+        }
+
+        enableElements();
+    }
+
+    function onChangeCategory(category) {
+        categoryElement.disabled = true;
+        subcategoryElement.disabled = true;
+        fetch('/api/categories/' + category)
+            .then(response => {
+                if (!response.ok) {
+                    resetElements();
+                    throw new Error('Bad Response');
+                }
+                return response.json();
+            }).then(jsonData => setSubcategories(jsonData));
+    }
+
+    categoryElement.addEventListener('change', event => {
+        onChangeCategory(event.target.value);
+    })
+}
