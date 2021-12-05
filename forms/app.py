b380471e0e344d598871e66c68b88ed4b1357be3
Kaya Koch <Kaya.Koch@gmail.com>
2021-12-05 14:39:33 +1100

    [forms] Select field validation

--- forms/app.py
+++ forms/app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, request, abort, jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField
-from wtforms.validators import InputRequired, DataRequired
+from wtforms.validators import InputRequired, DataRequired, ValidationError
 
 
 app = Flask(__name__)
@@ -13,6 +13,8 @@ category_data = (
     (23, 'alpha'),
     (42, 'beta'),
 )
+
+valid_categories = [i[0] for i in category_data]
 
 subcategory_data = (
     (23, 1, 'aaa'),
@@ -29,6 +31,14 @@ class BasicForm(FlaskForm):
     category = SelectField('Category', coerce=int)
     subcategory = SelectField('Subcategory', coerce=int)
 
+    def validate_subcategory(form, field):
+        options = get_subcategories_for_category(form.category.data)
+        if not options:
+            raise ValidationError('Bad category')
+
+        if field.data not in options:
+            raise ValidationError('Subcategory not valid for category')
+
 
 @app.route('/')
 def home():
@@ -40,7 +50,12 @@ def basic():
     form = BasicForm()
 
     form.category.choices = list(category_data)
-    form.subcategory.choices = get_subcategories_for_category(form.category.choices[0][0])
+
+    current_category = valid_categories[0]
+    if form.category.data and form.category.data in valid_categories:
+        current_category = form.category.data
+
+    form.subcategory.choices = get_subcategories_for_category(current_category)
 
     if form.validate_on_submit():
         print(f'category={form.category.data}({type(form.category.data)}) subcategory={form.subcategory.data}({type(form.subcategory.data)})')
